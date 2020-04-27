14a89c593837edc56a593cad16f141369a70ec4a
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2020-04-27 17:40:38 +0300

    Updates

---
+++ precache-manifest.85d64d1b0521be44e31e573469679d1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb7fc633526a8df0a1b329c89b244de7",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "82d5d411c596d8711c12",
+    "url": "/goit-react-hw-03-phonebook/static/css/2.f1d36a8f.chunk.css"
+  },
+  {
+    "revision": "fb7ba4b0c86cc5a8e80f",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.8e30bb39.chunk.css"
+  },
+  {
+    "revision": "82d5d411c596d8711c12",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.e7d474a1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.e7d474a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb7ba4b0c86cc5a8e80f",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.342bb253.chunk.js"
+  },
+  {
+    "revision": "3c7d6332fe8286c4c89d",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.b64ae70b.js"
+  }
+]);
